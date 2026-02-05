b5dd87daf0a9b2f317e0c664522ba5c91239643e
Programming Laptop 2 <aztechs157General@gmail.com>
2026-02-04 20:49:03 -0500

    Created intake subsystem skeleton

--- src/main/java/org/team157/robot/RobotContainer.java
+++ src/main/java/org/team157/robot/RobotContainer.java
@@ -27,7 +27,7 @@ import org.team157.robot.Constants.ModifierConstants;
 import org.team157.robot.generated.TunerConstants;
 import org.team157.robot.subsystems.DriveSystem;
 import org.team157.robot.subsystems.FlywheelSystem;
-
+import org.team157.robot.subsystems.IntakeSystem;
 import org.team157.robot.subsystems.TurretSystem;
 import org.team157.robot.subsystems.VisionSystem;
 
@@ -50,6 +50,7 @@ public class RobotContainer {
 
     public final FlywheelSystem flywheelSystem = new FlywheelSystem();
     public final TurretSystem turret = new TurretSystem();
+    public final IntakeSystem intake = new IntakeSystem();
 
     private final SendableChooser<Command> autoChooser;
 
@@ -143,11 +144,17 @@ public class RobotContainer {
         ////////////////////////////////////////////////////////
         /// FLYWHEEL COMMANDS
         ///////////////////////////////////////////////////////
-        
         flywheelSystem.setDefaultCommand(flywheelSystem.set(0));
 
         driverController.rightTrigger().whileTrue(flywheelSystem.setVelocity(RPM.of(60)));
+
+        ////////////////////////////////////////////////////////
+        /// INTAKE COMMANDS
+        ///////////////////////////////////////////////////////
+        intake.setDefaultCommand(intake.setDefault());
+
     }
+
 
     public double modifySpeed(final double speed) {
         final var modifier = 1 - driverController.getRightTriggerAxis() * ModifierConstants.PRECISION_DRIVE_MODIFIER;
