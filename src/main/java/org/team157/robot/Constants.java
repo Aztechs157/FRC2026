3f0d1400fd0f0c10eb286e6ec36711c9206de33f
Programming Laptop 2 <aztechs157General@gmail.com>
2026-02-27 21:21:46 -0500

    Restart tuning flywheel. - JM RG

--- src/main/java/org/team157/robot/Constants.java
+++ src/main/java/org/team157/robot/Constants.java
@@ -137,12 +137,12 @@ public final class Constants {
     // IDs of both motors powering the flywheel.
     public static final int MOTOR_ID = 20, FOLLOWER_MOTOR_ID = 21;
     // Closed-loop control values for the flywheel.
-    public static final double KP = 0.9, KI = 0, KD = 0.09;
-    public static final double KS = 0.0, KV = 0.37, KA = 0.37; 
+    public static final double KP = 2, KI = 0, KD = 0;
+    public static final double KS = 0.0, KV = 0.0, KA = 0.0; 
     public static final double SIM_KP = 1.57, SIM_KI = 0, SIM_KD = 0.157;
     public static final double SIM_KS = 0.0, SIM_KV = 0.37, SIM_KA = 0.27; // Determined via Reca.lc, tune and/or run SysID to verify these values.
     public static final AngularVelocity ANGULAR_VELOCITY = RPM.of(5800);
-    public static final AngularAcceleration ANGULAR_ACCELERATION = RotationsPerSecondPerSecond.of(3000);
+    public static final AngularAcceleration ANGULAR_ACCELERATION = RotationsPerSecondPerSecond.of(11600);
     // Gear ratio between the motor and the flywheel.
     public static final MechanismGearing GEARING = new MechanismGearing(GearBox.fromStages("17:17"));
     // Diameter of the flywheel, in meters.
