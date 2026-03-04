a5dab9c7807829296610c9db94d256e334f7154b
James Mwangi <void.backup.av@gmail.com>
2026-03-03 21:25:06 -0500

    Readd distanceToTargetFromTurret to VisionSystem to fix dynamic flywheel

--- src/main/java/org/team157/robot/subsystems/VisionSystem.java
+++ src/main/java/org/team157/robot/subsystems/VisionSystem.java
@@ -375,6 +375,8 @@ public class VisionSystem extends SubsystemBase {
      */
     public void setTargetParams(Pose2d targetPose, Pose2d robotPose) {
       distanceToTarget = PhotonUtils.getDistanceToPose(robotPose, targetPose);
+      distanceToTargetFromTurret = PhotonUtils.getDistanceToPose(robotPose.plus(ModelConstants.XY_ORIGIN_TO_TURRET_BASE_OFFSET), targetPose);
+
       // TODO: Continue work on implementation of momentum-based velocity calculation on new branch.
       // lastDistanceToTarget = distanceToTarget;
       // lastTrackedTime = NetworkTablesJNI.now();
