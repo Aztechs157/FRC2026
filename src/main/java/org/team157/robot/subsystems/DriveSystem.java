7d1cfa2dfed3ed51f30e58e8904253a1730d3083
Katie Silva <kts03290@gmail.com>
2026-03-04 22:30:03 +0100

    fixed minor typo - KS

--- src/main/java/org/team157/robot/subsystems/DriveSystem.java
+++ src/main/java/org/team157/robot/subsystems/DriveSystem.java
@@ -208,7 +208,7 @@ public class DriveSystem extends TunerSwerveDrivetrain implements Subsystem {
         AutoBuilder.configure(
                 () -> getState().Pose, // Supplier of current robot pose
                 this::resetPose, // Consumer for seeding pose against auto
-                () -> getState().Speeds, // Supplier of current robot speeds7
+                () -> getState().Speeds, // Supplier of current robot speeds
                 // Consumer of ChassisSpeeds and feedforwards to drive the robot
                 (speeds, feedforwards) -> setControl(
                         m_pathApplyRobotSpeeds.withSpeeds(speeds)
