d3f38af6e017b5c52ddcf61fc9d5f5bad673d677
James Mwangi <void.backup.av@gmail.com>
2026-03-27 18:24:59 -0400

    Refactor Hopper subsystem for AdvantageKit

--- src/main/java/org/team157/robot/subsystems/hopper/HopperSystem.java
+++ src/main/java/org/team157/robot/subsystems/hopper/HopperSystem.java
@@ -2,7 +2,7 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package org.team157.robot.subsystems;
+package org.team157.robot.subsystems.hopper;
 
 import static edu.wpi.first.units.Units.Inches;
 import static edu.wpi.first.units.Units.Kilograms;
