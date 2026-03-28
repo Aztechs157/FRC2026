bade9e1e9be078c8a0fb2aefcdf7749f592bb5b1
Programming Laptop 2 <aztechs157General@gmail.com>
2026-03-28 15:43:08 -0400

    updated turret limits and positions for cardinal directions, updated angle offset - KS, RG

--- src/main/java/org/team157/robot/subsystems/turret/TurretConstants.java
+++ src/main/java/org/team157/robot/subsystems/turret/TurretConstants.java
@@ -23,8 +23,8 @@ import edu.wpi.first.units.measure.Time;
 public final class TurretConstants {
     public static final int MOTOR_ID = 20;
     public static final int ENCODER_ID = 0;
-    public static final double MIN_ENCODER_POSITION = 0.99, MAX_ENCODER_POSITION = 0.014;
-    public static final double MIN_ANGLE = -179, MAX_ANGLE = 179;
+    public static final double MIN_ENCODER_POSITION = 0.996, MAX_ENCODER_POSITION = 0.014;
+    public static final double MIN_ANGLE = -178, MAX_ANGLE = 171;
     public static final double KP = 157, KI = 0, KD = 0;
     public static final double SIM_KP = 5, SIM_KI = 0, SIM_KD = 0;
     public static final AngularVelocity ANGULAR_VELOCITY = DegreesPerSecond.of(360), SIM_ANGULAR_VELOCITY = DegreesPerSecond.of(3.6);
@@ -32,9 +32,9 @@ public final class TurretConstants {
     public static final MechanismGearing GEARING = new MechanismGearing(GearBox.fromStages("28:1"));
     public static final Current CURRENT_LIMIT = Amps.of(40);
     public static final Time RAMP_RATE = Seconds.of(0.00157);
-    public static final Angle LOWER_SOFT_LIMIT = Degrees.of(-179), UPPER_SOFT_LIMIT = Degrees.of(179);
-    public static final Angle LOWER_HARD_LIMIT = Degrees.of(-179), UPPER_HARD_LIMIT = Degrees.of(179);
+    public static final Angle LOWER_SOFT_LIMIT = Degrees.of(-178), UPPER_SOFT_LIMIT = Degrees.of(171);
+    public static final Angle LOWER_HARD_LIMIT = Degrees.of(-178), UPPER_HARD_LIMIT = Degrees.of(171);
     // Angle offset to account for misalignment between turret zero position and robot forward, in degrees.
-    public static final double TURRET_ANGLE_OFFSET = 130;
+    public static final double TURRET_ANGLE_OFFSET = 168.5;
 
   }
