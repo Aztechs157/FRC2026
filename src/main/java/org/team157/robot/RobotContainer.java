94d811ff0a9c9cf4e45d62983ea319e0fac29b3a
Programming Laptop 2 <aztechs157General@gmail.com>
2026-03-31 20:16:34 -0400

    Tuned dynamic hood, increase trench zone sizes, and change default command to go to max angle when not in dynamic mode. -JM RG RN

--- src/main/java/org/team157/robot/RobotContainer.java
+++ src/main/java/org/team157/robot/RobotContainer.java
@@ -132,7 +132,7 @@ public class RobotContainer {
         intake.setDefaultCommand(intake.getDefault());
         hopper.setDefaultCommand(hopper.getDefault());
         uptake.setDefaultCommand(uptake.setDefault());
-        hood.setDefaultCommand(hood.getDefault());
+        hood.setDefaultCommand(hood.getDefault(drivetrain));
 
         // Run SysId routines when holding back/start and X/Y.
         // Note that each routine should be run exactly once in a single log.
