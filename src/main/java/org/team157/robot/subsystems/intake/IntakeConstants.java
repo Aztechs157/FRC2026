fc755360e89eb5c1b4d77c6e2b54022f3e139405
Programming Laptop 2 <aztechs157General@gmail.com>
2026-04-14 20:52:29 -0400

    Import and start tuning of AdvantageKit Swerve drivetrain. - JM JR RG RN SD eG

--- src/main/java/org/team157/robot/subsystems/intake/IntakeConstants.java
+++
@@ -1,6 +0,0 @@
-package org.team157.robot.subsystems.intake;
-
-public class IntakeConstants {
-    /** Roller motor IDs */
-    public static final int ROLLER_MOTOR_ID = 14, FOLLOWER_MOTOR_ID = 15;
-}
