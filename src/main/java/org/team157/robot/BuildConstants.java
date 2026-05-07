09551fa0ae0fa22acfcde9929ea9be29ac517b91
James Mwangi <void.backup.av@gmail.com>
2026-05-07 13:29:57 -0400

    Update build.gradle to use Red Hat autoformatter (less ugly) and delete unused txt file.

--- src/main/java/org/team157/robot/BuildConstants.java
+++ src/main/java/org/team157/robot/BuildConstants.java
@@ -1,17 +1,20 @@
 package org.team157.robot;
 
-/** Automatically generated file containing build version information. */
+/**
+ * Automatically generated file containing build version information.
+ */
 public final class BuildConstants {
-  public static final String MAVEN_GROUP = "";
-  public static final String MAVEN_NAME = "FRC2026";
-  public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 347;
-  public static final String GIT_SHA = "6a73be18bc8fe359e14f332ee35a74ffb315df58";
-  public static final String GIT_DATE = "2026-05-06 21:34:55 EDT";
-  public static final String GIT_BRANCH = "task/reexport-as-model";
-  public static final String BUILD_DATE = "2026-05-07 08:37:05 EDT";
-  public static final long BUILD_UNIX_TIME = 1778157425160L;
-  public static final int DIRTY = 1;
+    public static final String MAVEN_GROUP = "";
+    public static final String MAVEN_NAME = "FRC2026";
+    public static final String VERSION = "unspecified";
+    public static final int GIT_REVISION = 348;
+    public static final String GIT_SHA = "07baff9206b3c51a5657a25d6df174198e211405";
+    public static final String GIT_DATE = "2026-05-07 09:19:12 EDT";
+    public static final String GIT_BRANCH = "task/reexport-as-model";
+    public static final String BUILD_DATE = "2026-05-07 13:24:40 EDT";
+    public static final long BUILD_UNIX_TIME = 1778174680714L;
+    public static final int DIRTY = 1;
 
-  private BuildConstants() {}
+    private BuildConstants() {
+    }
 }
