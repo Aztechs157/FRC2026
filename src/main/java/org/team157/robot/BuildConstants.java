3a0121448822ec0deb5cc19e3e95fa164cb6437d
James Mwangi <void.backup.av@gmail.com>
2026-05-07 15:10:36 -0400

    Remove outdated models, rename Mantiis -> SunstoneV1, add SunstoneV2 model

--- src/main/java/org/team157/robot/BuildConstants.java
+++ src/main/java/org/team157/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "";
     public static final String MAVEN_NAME = "FRC2026";
     public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 351;
-    public static final String GIT_SHA = "bd679eb2915a85358a3a76bc5049d9ee764e6b21";
-    public static final String GIT_DATE = "2026-05-07 15:00:37 EDT";
+    public static final int GIT_REVISION = 356;
+    public static final String GIT_SHA = "6d29809a60e370016d1031acab5d400024a46ae3";
+    public static final String GIT_DATE = "2026-05-07 15:05:40 EDT";
     public static final String GIT_BRANCH = "task/reexport-as-model";
-    public static final String BUILD_DATE = "2026-05-07 15:05:02 EDT";
-    public static final long BUILD_UNIX_TIME = 1778180702599L;
-    public static final int DIRTY = 0;
+    public static final String BUILD_DATE = "2026-05-07 15:07:18 EDT";
+    public static final long BUILD_UNIX_TIME = 1778180838977L;
+    public static final int DIRTY = 1;
 
     private BuildConstants() {}
 }
