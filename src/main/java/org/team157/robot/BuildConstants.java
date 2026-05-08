074e2f24d2d15699dc4c3a2ce91622165fb10d8c
Aztechs157-Git <aztechs157General@gmail.com>
2026-05-08 16:13:55 -0400

    Misc. flywheel tuning. -JM

--- src/main/java/org/team157/robot/BuildConstants.java
+++ src/main/java/org/team157/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "FRC2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 343;
-  public static final String GIT_SHA = "43b005d25ca2ac75ec9b23061b25083ef2fce003";
-  public static final String GIT_DATE = "2026-05-01 19:07:48 EDT";
+  public static final int GIT_REVISION = 344;
+  public static final String GIT_SHA = "0ae370b8e86b6357360cdd0379248557bf1ede08";
+  public static final String GIT_DATE = "2026-05-05 16:10:13 EDT";
   public static final String GIT_BRANCH = "task/flywheel-reup";
-  public static final String BUILD_DATE = "2026-05-01 20:49:49 EDT";
-  public static final long BUILD_UNIX_TIME = 1777682989025L;
+  public static final String BUILD_DATE = "2026-05-05 20:26:20 EDT";
+  public static final long BUILD_UNIX_TIME = 1778027180132L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
--- src/main/java/org/team157/robot/subsystems/flywheel/FlywheelConstants.java
+++ src/main/java/org/team157/robot/subsystems/flywheel/FlywheelConstants.java
@@ -25,16 +25,16 @@ public final class FlywheelConstants {
   // IDs of both motors powering the flywheel.
   public static final int MOTOR_ID = 22, FOLLOWER_MOTOR_ID = 23;
   // Closed-loop control values for the flywheel.
-  public static final double KP = 3.57, KI = 0, KD = 0;
-  public static final double KS = 0.0, KV = 0.37, KA = 0.27;
+  public static final double KP = 0.005, KI = 0, KD = 0.0;
+  public static final double KS = 0.0, KV = 0.116, KA = 0.05;
   public static final double SIM_KP = 1.57, SIM_KI = 0, SIM_KD = 0.157;
   public static final double SIM_KS = 0.0,
       SIM_KV = 0.37,
-      SIM_KA = 0.27; // Determined via Reca.lc, tune and/or run
+      SIM_KA = 0.25; // Determined via Reca.lc, tune and/or run
   // SysID to verify these values.
   public static final AngularVelocity ANGULAR_VELOCITY = RPM.of(5800);
   public static final AngularAcceleration ANGULAR_ACCELERATION =
-      RotationsPerSecondPerSecond.of(5800);
+      RotationsPerSecondPerSecond.of(3600);
   // Gear ratio between the motor and the flywheel.
   public static final MechanismGearing GEARING = new MechanismGearing(GearBox.fromStages("17:17"));
   // Diameter of the flywheel, in meters.
