531e0f1c40772b080ba7102c19598d60e217b7ba
James Mwangi <void.backup.av@gmail.com>
2026-05-13 13:42:05 -0400

    Correct back camera FOVs and update camera locations in model config files.

--- src/main/java/org/team157/robot/BuildConstants.java
+++ src/main/java/org/team157/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "";
     public static final String MAVEN_NAME = "FRC2026";
     public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 362;
-    public static final String GIT_SHA = "76a6e0ee470a3f8c48fe1ccc080becf33d4ee1b5";
-    public static final String GIT_DATE = "2026-05-13 10:29:39 EDT";
+    public static final int GIT_REVISION = 363;
+    public static final String GIT_SHA = "53f6929b0f6dbe6186720b72cbc75aa6a27df032";
+    public static final String GIT_DATE = "2026-05-13 12:19:08 EDT";
     public static final String GIT_BRANCH = "task/reexport-as-model";
-    public static final String BUILD_DATE = "2026-05-13 12:07:43 EDT";
-    public static final long BUILD_UNIX_TIME = 1778688463850L;
+    public static final String BUILD_DATE = "2026-05-13 13:39:20 EDT";
+    public static final long BUILD_UNIX_TIME = 1778693960897L;
     public static final int DIRTY = 1;
 
     private BuildConstants() {}
