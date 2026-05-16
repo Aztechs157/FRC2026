ab7313c1da00fbe5e77021e4e287a4025a1c6632
Programming Laptop 2 <aztechs157General@gmail.com>
2026-05-15 20:46:44 -0400

    created layout - RN

--- src/main/java/org/team157/robot/BuildConstants.java
+++ src/main/java/org/team157/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "";
     public static final String MAVEN_NAME = "FRC2026";
     public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 366;
-    public static final String GIT_SHA = "2bdd41328abdfc8999eab7d5cad6e80f4c55241c";
-    public static final String GIT_DATE = "2026-05-15 11:56:30 EDT";
-    public static final String GIT_BRANCH = "task/reexport-as-model";
-    public static final String BUILD_DATE = "2026-05-15 15:57:29 EDT";
-    public static final long BUILD_UNIX_TIME = 1778875049705L;
-    public static final int DIRTY = 0;
+    public static final int GIT_REVISION = 368;
+    public static final String GIT_SHA = "86616d12a7747a55925f3732096dac5d76dbbfca";
+    public static final String GIT_DATE = "2026-05-15 16:39:07 EDT";
+    public static final String GIT_BRANCH = "dev";
+    public static final String BUILD_DATE = "2026-05-15 16:48:49 EDT";
+    public static final long BUILD_UNIX_TIME = 1778878129780L;
+    public static final int DIRTY = 1;
 
     private BuildConstants() {}
 }
