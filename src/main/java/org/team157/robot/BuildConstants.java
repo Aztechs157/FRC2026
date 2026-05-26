5b201241b81ea7d577faeef4f990a3cd03e95e4d
Aztechs157-Git <aztechs157General@gmail.com>
2026-05-26 19:44:57 -0400

    update layout according to pr comments - rn

--- src/main/java/org/team157/robot/BuildConstants.java
+++ src/main/java/org/team157/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "";
     public static final String MAVEN_NAME = "FRC2026";
     public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 370;
-    public static final String GIT_SHA = "280275c40f6542433273b0f1945a6bff02c57f20";
-    public static final String GIT_DATE = "2026-05-19 19:03:05 EDT";
-    public static final String GIT_BRANCH = "flywheel-auto-tuning";
-    public static final String BUILD_DATE = "2026-05-19 20:11:37 EDT";
-    public static final long BUILD_UNIX_TIME = 1779235897649L;
+    public static final int GIT_REVISION = 385;
+    public static final String GIT_SHA = "7b20a18f0a2730e6f1d0ce8007d9a069dc9af0e3";
+    public static final String GIT_DATE = "2026-05-26 16:26:13 EDT";
+    public static final String GIT_BRANCH = "advantageScopeLayout";
+    public static final String BUILD_DATE = "2026-05-26 18:49:47 EDT";
+    public static final long BUILD_UNIX_TIME = 1779835787335L;
     public static final int DIRTY = 1;
 
     private BuildConstants() {}
