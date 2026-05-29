94acef23fafee2a5657c798fc798ced8c9542c3b
Aztechs157 <aztechs157general@gmail.com>
2026-05-29 18:46:00 -0400

    Update at 'Fri May 29 18:45:59 EDT 2026'

--- src/main/java/org/team157/robot/BuildConstants.java
+++ src/main/java/org/team157/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "";
     public static final String MAVEN_NAME = "FRC2026";
     public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 424;
-    public static final String GIT_SHA = "bf151e8b6e78b5ab0d76e62ad320c57f0a2d575c";
-    public static final String GIT_DATE = "2026-05-29 18:36:27 EDT";
+    public static final int GIT_REVISION = 425;
+    public static final String GIT_SHA = "e7cf632a2266b49552b13a98783ec93861224d21";
+    public static final String GIT_DATE = "2026-05-29 18:45:27 EDT";
     public static final String GIT_BRANCH = "event-bc2026";
-    public static final String BUILD_DATE = "2026-05-29 18:36:27 EDT";
-    public static final long BUILD_UNIX_TIME = 1780094187846L;
+    public static final String BUILD_DATE = "2026-05-29 18:45:28 EDT";
+    public static final long BUILD_UNIX_TIME = 1780094728171L;
     public static final int DIRTY = 0;
 
     private BuildConstants() {}
