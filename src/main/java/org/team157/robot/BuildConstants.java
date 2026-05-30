2166a078274b97b57c67b396f219a6cf149d86d5
Aztechs157 <aztechs157general@gmail.com>
2026-05-30 19:46:44 -0400

    fixed trench dot autos - JM, RN

--- src/main/java/org/team157/robot/BuildConstants.java
+++ src/main/java/org/team157/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "";
     public static final String MAVEN_NAME = "FRC2026";
     public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 438;
-    public static final String GIT_SHA = "8681b6e03aa298506e0f99734906f333934c841b";
-    public static final String GIT_DATE = "2026-05-30 16:48:46 EDT";
+    public static final int GIT_REVISION = 440;
+    public static final String GIT_SHA = "16a53c58a4644c4351d88212bbed8bb186f8ca83";
+    public static final String GIT_DATE = "2026-05-30 17:52:44 EDT";
     public static final String GIT_BRANCH = "bcDotAutos";
-    public static final String BUILD_DATE = "2026-05-30 16:51:40 EDT";
-    public static final long BUILD_UNIX_TIME = 1780174300444L;
-    public static final int DIRTY = 0;
+    public static final String BUILD_DATE = "2026-05-30 18:23:56 EDT";
+    public static final long BUILD_UNIX_TIME = 1780179836274L;
+    public static final int DIRTY = 1;
 
     private BuildConstants() {}
 }
