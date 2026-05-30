76cc44e9a27dd1c16164e5b03404ffd364b1c6c8
James Mwangi <void.backup.av@gmail.com>
2026-05-30 10:57:34 -0400

    Update at 'Sat May 30 10:57:34 EDT 2026'

--- src/main/java/org/team157/robot/BuildConstants.java
+++ src/main/java/org/team157/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "";
     public static final String MAVEN_NAME = "FRC2026";
     public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 428;
-    public static final String GIT_SHA = "aca6a9aeedd55274c4601214a8144e9c8bcf809e";
-    public static final String GIT_DATE = "2026-05-29 20:43:11 EDT";
+    public static final int GIT_REVISION = 429;
+    public static final String GIT_SHA = "0d35a2dcad2f09083e18dca39b1854ea33d823c9";
+    public static final String GIT_DATE = "2026-05-30 09:30:16 EDT";
     public static final String GIT_BRANCH = "event-bc2026";
-    public static final String BUILD_DATE = "2026-05-30 08:47:16 EDT";
-    public static final long BUILD_UNIX_TIME = 1780145236707L;
-    public static final int DIRTY = 1;
+    public static final String BUILD_DATE = "2026-05-30 09:30:16 EDT";
+    public static final long BUILD_UNIX_TIME = 1780147816491L;
+    public static final int DIRTY = 0;
 
     private BuildConstants() {}
 }
--- src/main/java/org/team157/robot/RobotContainer.java
+++ src/main/java/org/team157/robot/RobotContainer.java
@@ -488,7 +488,7 @@ public class RobotContainer {
 
     /** Enables controller rumble when 2 seconds remain in the current shift. */
     public void setRumble() {
-        if (hubStatus.isShiftAboutToEnd(2)) {
+        if (hubStatus.isShiftAboutToEnd(2) || (hubStatus.isShiftAboutToEnd(7) && DriverStation.isTeleop())) {
             driverController.setRumble(RumbleType.kLeftRumble, 1);
             driverController.setRumble(RumbleType.kRightRumble, 1);
             operatorController.setRumble(RumbleType.kLeftRumble, 1);
