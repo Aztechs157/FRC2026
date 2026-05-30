c739871e84c7b5b109553d79ec74eaacb95730c1
Aztechs157-Git <aztechs157General@gmail.com>
2026-05-30 15:09:33 -0400

    add more dot autos  (also accidetnally moved the hp linked point just a touch but the difference is negligable)- RN

--- src/main/java/org/team157/robot/BuildConstants.java
+++ src/main/java/org/team157/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "";
     public static final String MAVEN_NAME = "FRC2026";
     public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 432;
-    public static final String GIT_SHA = "76cc44e9a27dd1c16164e5b03404ffd364b1c6c8";
-    public static final String GIT_DATE = "2026-05-30 10:57:34 EDT";
-    public static final String GIT_BRANCH = "event-bc2026";
-    public static final String BUILD_DATE = "2026-05-30 10:57:35 EDT";
-    public static final long BUILD_UNIX_TIME = 1780153055178L;
-    public static final int DIRTY = 0;
+    public static final int GIT_REVISION = 436;
+    public static final String GIT_SHA = "5439259126062d10406f38537ba522dd932b9b9a";
+    public static final String GIT_DATE = "2026-05-30 14:30:09 EDT";
+    public static final String GIT_BRANCH = "bcDotAutos";
+    public static final String BUILD_DATE = "2026-05-30 15:04:02 EDT";
+    public static final long BUILD_UNIX_TIME = 1780167842343L;
+    public static final int DIRTY = 1;
 
     private BuildConstants() {}
 }
