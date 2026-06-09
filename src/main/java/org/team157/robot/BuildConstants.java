196c26e89aa2970e923cf3e06f22b762764b9b18
Aztechs157-Git <aztechs157General@gmail.com>
2026-06-09 19:45:00 -0400

    Minor Auto position changes - RN

--- src/main/java/org/team157/robot/BuildConstants.java
+++ src/main/java/org/team157/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "";
     public static final String MAVEN_NAME = "FRC2026";
     public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 438;
-    public static final String GIT_SHA = "8681b6e03aa298506e0f99734906f333934c841b";
-    public static final String GIT_DATE = "2026-05-30 16:48:46 EDT";
+    public static final int GIT_REVISION = 439;
+    public static final String GIT_SHA = "e85910f41693346cba7fc0b3d8cd7bf7d05477c3";
+    public static final String GIT_DATE = "2026-05-30 17:00:45 EDT";
     public static final String GIT_BRANCH = "bcDotAutos";
-    public static final String BUILD_DATE = "2026-05-30 16:51:40 EDT";
-    public static final long BUILD_UNIX_TIME = 1780174300444L;
-    public static final int DIRTY = 0;
+    public static final String BUILD_DATE = "2026-05-30 17:15:42 EDT";
+    public static final long BUILD_UNIX_TIME = 1780175742619L;
+    public static final int DIRTY = 1;
 
     private BuildConstants() {}
 }
