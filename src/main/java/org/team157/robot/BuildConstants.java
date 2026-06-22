49cad3ca44da671be209d313ca36b3ba3dcc952c
James Mwangi <void.backup.av@gmail.com>
2026-06-22 17:41:29 -0400

    Autoformatter diff + Remove Current tab due to redundancy with other merchanism tabs + Move video tab left by 1

--- src/main/java/org/team157/robot/BuildConstants.java
+++ src/main/java/org/team157/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "";
     public static final String MAVEN_NAME = "FRC2026";
     public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 441;
-    public static final String GIT_SHA = "2166a078274b97b57c67b396f219a6cf149d86d5";
-    public static final String GIT_DATE = "2026-05-30 19:46:44 EDT";
-    public static final String GIT_BRANCH = "event-bc2026";
-    public static final String BUILD_DATE = "2026-06-09 19:58:07 EDT";
-    public static final long BUILD_UNIX_TIME = 1781049487030L;
-    public static final int DIRTY = 1;
+    public static final int GIT_REVISION = 444;
+    public static final String GIT_SHA = "1fbe3218ab8328d60c2133e518633d6f97069355";
+    public static final String GIT_DATE = "2026-06-10 17:07:32 EDT";
+    public static final String GIT_BRANCH = "task/update-advantagescope-layout";
+    public static final String BUILD_DATE = "2026-06-22 17:31:55 EDT";
+    public static final long BUILD_UNIX_TIME = 1782163915186L;
+    public static final int DIRTY = 0;
 
     private BuildConstants() {}
 }
